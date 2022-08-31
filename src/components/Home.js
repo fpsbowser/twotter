9b7c3272dd90a35daf08fd9d5a453252a1ffb356
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-08-31 18:50:38 -0400

    added user details and tweets render

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Tweets from "./Tweets";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home = (props) => {
+  const { user, username } = props;
+  // console.log(user);
+  return (
+    <div className="home-container">
+      <h1>Home</h1>
+      <div className="user-info">
+        <h1>{user.displayName ? user.displayName : username}</h1>
+        <h2>{user.email}</h2>
+      </div>
+      <Tweets />
+    </div>
+  );
 };
 
 export default Home;
