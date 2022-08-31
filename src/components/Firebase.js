f1b9730cd7a099f728b48620cf629b9f5e85c574
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-08-31 18:48:05 -0400

    added auth

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
@@ -14,5 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth();
 
-export { app, db };
+export { app, db, auth };
