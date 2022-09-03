6f1d3f03dc2499ff03cc8690d9f7f8f4e8dcb7c3
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-09-02 21:00:50 -0400

    added handleclick function and refactored firebase query

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -1,28 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore";
 import { db } from "./Firebase";
 
-function Tweets() {
+function Tweets(props) {
+  const { setUserId, setViewProfile } = props;
   const [tweets, setTweets] = useState([]);
 
-  const fetchTweets = async () => {
-    const querySnapshot = await getDocs(collection(db, "tweets"));
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, "=>", doc.data().tweet);
-      setTweets((prevState) => [...prevState, doc]);
-    });
-  };
-
   useEffect(() => {
-    fetchTweets();
+    // listen to firestore for changes to update tweet state
+    const q = query(collection(db, "tweets"), orderBy("timestamp", "desc"));
+    const snap = onSnapshot(q, (querySnapshot) => {
+      const currentTweets = [];
+      querySnapshot.forEach((doc) => {
+        currentTweets.push(doc);
+        setTweets(currentTweets);
+      });
+    });
   }, []);
-  console.log(tweets);
+  // console.log(tweets);
+
+  function handleClick(e) {
+    console.log(e.target.id);
+    setUserId(e.target.id);
+    setViewProfile(true);
+  }
+
   return (
     <div className="tweet-container">
       {tweets.map((tweet) => {
         return (
           <div className="tweet-card" key={tweet.id}>
             <h3>{tweet.data().tweet}</h3>
+            <h5 onClick={handleClick} id={tweet.data().ownerId}>
+              {tweet.data().username}
+            </h5>
+            <h5>{`${new Date(tweet.data().timestamp.seconds * 1000)}`}</h5>
           </div>
         );
       })}
@@ -31,3 +44,12 @@ function Tweets() {
 }
 
 export default Tweets;
+
+// fetchTweets();
+// const fetchTweets = async () => {
+//   const querySnapshot = await getDocs(collection(db, "tweets"));
+//   querySnapshot.forEach((doc) => {
+//     console.log(doc.id, "=>", doc.data().tweet);
+//     setTweets((prevState) => [...prevState, doc]);
+//   });
+// };
