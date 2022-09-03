8ea33bd7ef157ef41a16f08bc355b0f3b7267497
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-09-02 20:58:46 -0400

    added conditional rendering for profile and added compose component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import Compose from "./Compose";
+import Profile from "./Profile";
 import Tweets from "./Tweets";
 
 const Home = (props) => {
   const { user, username } = props;
+
+  const [viewProfile, setViewProfile] = useState(false);
+  const [userId, setUserId] = useState("");
   // console.log(user);
-  return (
-    <div className="home-container">
-      <h1>Home</h1>
-      <div className="user-info">
-        <h1>{user.displayName ? user.displayName : username}</h1>
-        <h2>{user.email}</h2>
+  if (!viewProfile) {
+    console.log(user.uid);
+    return (
+      <div className="home-container">
+        <h1>Home</h1>
+        <div className="user-info">
+          <h1>{user.displayName ? user.displayName : username}</h1>
+          <h2>{user.email}</h2>
+        </div>
+        <Tweets setUserId={setUserId} setViewProfile={setViewProfile} />
+        <Compose />
       </div>
-      <Tweets />
-    </div>
-  );
+    );
+  } else {
+    return <Profile userId={userId} setViewProfile={setViewProfile} />;
+  }
 };
 
 export default Home;
