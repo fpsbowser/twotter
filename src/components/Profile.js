6515e1d05f4a0dff011ea6f7923cc228fe71593c
Dakota <36649413+fpsbowser@users.noreply.github.com>
2022-09-08 18:38:47 -0400

    updated JSX with CSS

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,49 +1,64 @@
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "./Firebase";
 
 function Profile(props) {
-  const { userId, setViewProfile } = props;
+  const { userId, user, profileUsername, setSignedInUser, signedInUser } =
+    props;
   const [tweets, setTweets] = useState([]);
-
-  function handleClick() {
-    setViewProfile(false);
-  }
+  // const [username, setUsername] = useState("");
 
   async function fetchUsersTweets() {
     const tweetRef = collection(db, "tweets");
-    const q = query(tweetRef, where("ownerId", "==", `${userId}`));
+    const q = query(
+      tweetRef,
+      where("ownerId", "==", `${userId}`),
+      orderBy("timestamp", "desc")
+    );
     const userTweets = [];
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-      //   console.log(doc.id, "=>", doc.data());
+      // console.log(doc.id, "=>", doc.data().username);
       userTweets.push(doc);
       setTweets(userTweets);
     });
   }
 
   useEffect(() => {
+    user.displayName === profileUsername
+      ? setSignedInUser(true)
+      : setSignedInUser(false);
     fetchUsersTweets();
-  }, []);
+  }, [tweets]);
 
   return (
-    <div>
-      <h2>Profile</h2>
-      <h2>{userId}</h2>
-      <div className="users-tweets">
-        {tweets.map((tweet) => {
-          return (
-            <div className="tweet-card" key={tweet.id}>
-              <h3>{tweet.data().tweet}</h3>
-              <h5 onClick={handleClick} id={tweet.data().ownerId}>
-                {tweet.data().username}
-              </h5>
-              <h5>{`${new Date(tweet.data().timestamp.seconds * 1000)}`}</h5>
-            </div>
-          );
-        })}
+    <div className="tweet-container">
+      <div className="tweet-container-header">
+        <h2 className="tweet-container-header-text">
+          {signedInUser ? `your twots` : `twots from ${profileUsername}`}
+        </h2>
       </div>
-      <button onClick={handleClick}>Home</button>
+      {tweets.map((tweet) => {
+        return (
+          <div className="tweet-card" key={tweet.id}>
+            <div className="tweet-header">
+              <img
+                src={require("../assets/account-circle-outline.png")}
+                alt="profile pic"
+                id="tweet-profile-pic"
+              />
+              <p className="profile-tweet-owner" id={tweet.data().ownerId}>
+                {tweet.data().username}
+              </p>
+            </div>
+
+            <p className="tweet-text">{tweet.data().tweet}</p>
+            <p className="tweet-date">{`${new Date(
+              tweet.data().timestamp.seconds * 1000
+            )}`}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
